ec9edc4c2e6186d48162962cd1132dc09b07f82e
MarioLin <mario.lin@berkeley.edu>
2017-05-13 15:26:48 -0700

    Second view controller

---
+++ mamawhatscooking/Pods/Target Support Files/Pods-mamawhatscooking/Pods-mamawhatscooking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mamawhatscooking : NSObject
+@end
+@implementation PodsDummy_Pods_mamawhatscooking
+@end
---
+++ mamawhatscooking/mamawhatscooking/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  mamawhatscooking
+//
+//  Created by Mario Lin on 5/13/17.
+//  Copyright © 2017 Mario Lin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    required init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+}
