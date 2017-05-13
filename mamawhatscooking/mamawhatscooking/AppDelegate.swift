2e51db5b0c38d8542a3cbcef759de62e3bb90b85
MarioLin <mario.lin@berkeley.edu>
2017-05-13 15:19:01 -0700

    set up pods

---
+++ mamawhatscooking/mamawhatscooking/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  mamawhatscooking
+//
+//  Created by Mario Lin on 5/13/17.
+//  Copyright © 2017 Mario Lin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
