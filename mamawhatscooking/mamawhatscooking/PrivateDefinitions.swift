295d4ed133093038e2414fd40429e31d87969cb7
MarioLin <mario.lin@berkeley.edu>
2017-07-31 19:05:11 -0700

    Push placeholder Private Definitions

---
+++ mamawhatscooking/mamawhatscooking/PrivateDefinitions.swift
@@ -0,0 +1,12 @@
+//
+//  PrivateDefinitions.swift
+//  mamawhatscooking
+//
+//  Created by Mario Lin on 6/14/17.
+//  Copyright © 2017 Mario Lin. All rights reserved.
+//
+
+import Foundation
+
+let yummly_app_key = "{your app key here}" // placeholders to hide app_key
+let yummly_app_id = "{your app id here}" // placeholders to hide app_id
